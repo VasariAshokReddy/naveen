8d0a16b53496845b10094df6ca072a0a6cf96256
SaralaBhadraiah <ashokreddy.va5@gmail.com>
2020-06-14 14:43:13 +0530

    Product 2 is added

--- SpringJavaConfigEx/src/org/ashok/product/config/AppConfiguration.java
+++ SpringJavaConfigEx/src/org/ashok/product/config/AppConfiguration.java
@@ -14,6 +14,10 @@ public class AppConfiguration {
 		pro.setProductId(1);
 		pro.setProductName("LG");
 		pro.setProductPrice(1123.0);
+		
+		pro.setProductId(2);
+		pro.setProductName("IPhone");
+		pro.setProductPrice(50000.0);
 		return pro;
 
 	}
